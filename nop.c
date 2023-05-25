1c7f633eb6ca67e773137fbf6f1d2c2f53b43116
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-26 02:36:28 +0300

    Task 5: nop

--- nop.c
+++ nop.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 /**
   *nop- nothing
-  *@stack: stack head
+  *@head: stack head
   *@line_number: line_number
   *Return: no return
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **head, unsigned int line_number)
 {
 	(void) line_number;
-	(void) stack;
+	(void) head;
 }
