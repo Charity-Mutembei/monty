611ba7533b5d4cdb336e8bae7ae1d4845e777152
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-26 04:35:22 +0300

    Task 15 - see 001

--- queue.c
+++ queue.c
@@ -19,22 +19,8 @@ void f_stack(stack_t **stack, unsigned int line_number)
  */
 void f_queue(stack_t **stack, unsigned int line_number)
 {
-stack_t *current;
-
-(void)line_number;
-
-if (*stack == NULL || (*stack)->next == NULL)
-return;
-
-current = *stack;
-while (current->next != NULL)
-current = current->next;
-
-current->next = *stack;
-(*stack)->prev = current;
-*stack = (*stack)->next;
-(*stack)->prev->next = NULL;
-(*stack)->prev = NULL;
-stack_second.sight = 1; /* Set the queue flag to indicate queue mode */
+    (void)stack;
+	(void)line_number;
+	stack_second.sight = 1;
 }
 
