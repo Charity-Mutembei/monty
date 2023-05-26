4cf7a958b9a6b4e4a1914290b8ab5d712b13ae05
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-05-26 04:02:27 +0300

    Task 15: Stack, queue

--- queue.c
+++ queue.c
@@ -1,12 +1,26 @@
 #include "monty.h"
+
 /**
- * queue - Set the data format to queue (FIFO)
+ * f_stack - Set the data format to stack (LIFO)
+ * @stack: Stack head
+ * @line_number: Line number
+ */
+void f_stack(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+/* No action required as stack is the default behavior */
+}
+
+/**
+ * f_queue - Set the data format to queue (FIFO)
  * @stack: Stack head
  * @line_number: Line number
  */
 void f_queue(stack_t **stack, unsigned int line_number)
 {
 stack_t *current;
+
 (void)line_number;
 
 if (*stack == NULL || (*stack)->next == NULL)
@@ -23,3 +37,4 @@ current->next = *stack;
 (*stack)->prev = NULL;
 stack_second.sight = 1; /* Set the queue flag to indicate queue mode */
 }
+
